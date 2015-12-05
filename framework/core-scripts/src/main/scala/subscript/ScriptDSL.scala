9eef3d947e41c3e890bca10c362e710bdcde4c6b
Anatoliy Kmetyuk <anatoliykmetyuk@gmail.com>
2015-12-05 17:40:30 +0200

    DSL method for map dataflow

--- framework/core-scripts/src/main/scala/subscript/ScriptDSL.scala
+++ framework/core-scripts/src/main/scala/subscript/ScriptDSL.scala
@@ -3,6 +3,7 @@ package subscript
 import subscript.language
 
 import subscript.vm._
+import subscript.vm.model.callgraph._
 
 import scala.util.Try
 
@@ -14,5 +15,7 @@ object ScriptDSL {
       var s_node: N_call[Any] = null
       do @{s_node = there.asInstanceOf[N_call[Any]]}: s then t(s_node.$success)^ else e(s_node.$failure)^
 
+    _dataflow_map[T](s: ScriptNode[Any], f: T => Any) =
+      @{there.onSuccess {`script`.$ = there.asInstanceOf[ScriptResultHolder[Any]].$.map(r => f(r.asInstanceOf[T]))} }: s
 
 }
