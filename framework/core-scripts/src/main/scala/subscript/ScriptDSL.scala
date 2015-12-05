e5b1057bfb5534e68fcc17f009061146a8007956
Anatoliy Kmetyuk <anatoliykmetyuk@gmail.com>
2015-12-05 21:51:05 +0200

    Dataflow bug fix: it cannot be looped any more

--- framework/core-scripts/src/main/scala/subscript/ScriptDSL.scala
+++ framework/core-scripts/src/main/scala/subscript/ScriptDSL.scala
@@ -10,11 +10,12 @@ import scala.util.Try
 /** Contains DSL scripts. */
 object ScriptDSL {
 
-  script..
-    _dataflow(s: ScriptNode[Any], t: Any => ScriptNode[Any], e: Throwable => ScriptNode[Any]) =
-      var s_node: N_call[Any] = null
-      do @{s_node = there.asInstanceOf[N_call[Any]]}: s then t(s_node.$success)^ else e(s_node.$failure)^
+  def _dataflow(s: ScriptNode[Any], t: Any => ScriptNode[Any], e: Throwable => ScriptNode[Any]): ScriptNode[Any] = {
+    var s_node: N_call[Any] = null
+    ([do @{s_node = there.asInstanceOf[N_call[Any]]}: s then t(s_node.$success)^ else e(s_node.$failure)^])
+  }
 
+  script..
     _dataflow_map[T](s: ScriptNode[Any], f: T => Any) =
       @{there.onSuccess {`script`.$ = there.asInstanceOf[ScriptResultHolder[Any]].$.map(r => f(r.asInstanceOf[T]))} }: s
 
