9c6f44c98644a1d8acce76acfa34fc2dd854aca6
Anatoliy Kmetyuk <anatoliykmetyuk@gmail.com>
2016-02-08 14:24:52 +0200

    Some tests for #29

--- framework/core-scripts/src/test/scala/subscript/BugsSuite.scala
+++ framework/core-scripts/src/test/scala/subscript/BugsSuite.scala
@@ -22,6 +22,25 @@ class BugsSuite extends FlatSpec with Matchers
 
   "#28" should "work" in {
     script s = [^1^^1 ^2^^2]^^
+    runScript(s).$ shouldBe Success(List((1, 2)))
   }
+
+  "#29" should "be possible to call nice while with prefixed simple expression" in {
+    var i = 0
+    script s = var flag = false
+               [while: !flag {!i += 1!} {!if (i >= 3) flag = true!}]
+  
+    runScript(s)
+    i shouldBe 3
+  }
+
+  it should "be possible to use prefixed expressions in nice script calls" in {
+    script..
+      a(x: Boolean) = if x then ^1 else ^2
+      b = a: !true
+
+    runScript(b).$ shouldBe Success(2)
+  }
+
 
 }
