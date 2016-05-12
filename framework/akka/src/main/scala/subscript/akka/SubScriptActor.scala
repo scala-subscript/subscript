89be2d3dbbd07bb175bf39c02f1ef9d5f098f79b
Anatoliy Kmetyuk <anatoliykmetyuk@gmail.com>
2016-05-12 15:14:32 +0300

    New syntax mechanics for actors (dataflow). Triggers can now return values

--- framework/akka/src/main/scala/subscript/akka/SubScriptActor.scala
+++ framework/akka/src/main/scala/subscript/akka/SubScriptActor.scala
@@ -1,5 +1,8 @@
 package subscript.akka
+
 import subscript.language
+import subscript.Predef._
+import subscript.objectalgebra.Trigger
 
 import subscript.DSL._
 import scala.collection.mutable.ListBuffer
@@ -9,6 +12,78 @@ import subscript.vm.model.template._
 import subscript.vm.model.template.concrete._
 import akka.actor._
 import subscript.vm.model.callgraph._
+
+
+trait SubScriptActorPlus extends Actor {
+
+  val runner    = SSARunnerV1Scheduler
+  val terminate = new Trigger
+
+  // Ability to handle a message is defined by a sender and a handler partial function. Message is communicated to the parent dataflow via trigger.
+  // `Either` allows to check via a predefined ActorRef or accept an arbitrary ActorRef as a parameter, so that it will be set to a `sender` later.
+  private val handlers = ListBuffer[(Either[ActorRef, ActualOutputParameter[ActorRef]], Trigger, PartialFunction[Any, Script[Any]])]()
+
+  // Scripts
+  script..
+    live: Any
+    die       = {!if (context ne null) context stop self!}
+    lifecycle = [live || terminate] ; die
+
+    registerHandler(act: Either[ActorRef, ActualOutputParameter[ActorRef]]) =
+      val vt         = new Trigger  // The value sent from `act` will pop from `vt` and will be returned from this script to be handled by the parent dataflow
+      val handlerFun = here.ancestor(9).getProperty[String, PartialFunction[Any, Script[Any]]]("then").get  // PartialFunction from the dataflow this `act` is LHS of. Will handle the matching messages. TBD: ancestor(n) should be replaced by a more descriptive methods, no guessing on the argument `n` should be done.
+      val handler    = (act, vt, handlerFun)  // Everything together
+
+      do handlers += handler  // Save the actor, the trigger and the handler function of the dataflow
+      [vt ~~(msg)~~> [do handlers -= handler; ^msg]]^  // Wait for a suitable message to pop from `vt`, then deregister the handler and return the message to be handled normally by the dataflow.
+
+  implicit script..
+    actorRef2script(a: ActorRef)        = registerHandler(Left ( a))
+    actorRefParam2script(??a: ActorRef) = registerHandler(Right(?a))
+
+  
+  // Callbacks
+  override def aroundPreStart() {
+    Debug.info(s"$this aroundPreStart INIT")  
+    runner.launch([lifecycle])
+    super.aroundPreStart()
+    Debug.info(s"$this aroundPreStart EXIT")
+  }
+
+  override def aroundReceive(receive: Actor.Receive, msg: Any) {
+    synchronized {
+      sendSynchronizationMessage(this)
+      wait()
+    }
+
+    // Check the incoming message for appropriate sender and ability to be handled by one of the handlers
+    // Previously was in handlers.synchronized. If something goes wrong, wrap it back in it.
+    handlers.collectFirst {
+      case (Left (actorRef     ), trigger, handler) if actorRef           == sender  && handler.isDefinedAt(msg) => trigger.trigger(msg)
+      case (Right(actorRefParam), trigger, handler) if actorRefParam.matches(sender) && handler.isDefinedAt(msg) => actorRefParam.transferFunction(sender); trigger.trigger(msg)
+    } match { // Was the message handled?
+      case None    => super.aroundReceive( receive        , msg); Debug.info(s"$this aroundReceive did NOT handle msg   sender: $sender msg: $msg")
+      case Some(_) => super.aroundReceive({case _: Any =>}, msg); Debug.info(s"$this aroundReceive handled  sender: $sender msg: $msg")      
+    }
+  }
+
+  override def aroundPostStop() {
+    terminate.trigger
+    super.aroundPostStop()
+  }
+
+  def receive: Actor.Receive = {case _ =>}
+
+  def sendSynchronizationMessage(lock: AnyRef) {
+    val vm = runner.executor
+    vm insert SynchronizationMessage(vm.rootNode, lock)
+  }
+}
+
+case class SynchronizationMessage(node: CallGraphNode, lock: AnyRef) extends CallGraphMessageN {
+  type N = CallGraphNode
+  override def priority = PRIORITY_InvokeFromET - 1  // After all actors are launched
+}
 
 // was: import scala.actors.Logger
 object Debug extends Logger("") {}
@@ -26,75 +101,4 @@ class Logger(tag: String) {
   def doInfo   (b: => Unit) = if (lev > 2) b
   def doWarning(b: => Unit) = if (lev > 1) b
   def doError  (b: => Unit) = if (lev > 0) b
-}
-trait SubScriptActor extends Actor {
-
-  val runner: SubScriptActorRunner = SSARunnerV1Scheduler
-
-  private object Terminator { // TBD: find better name; something with Blocker
-    var executor: EventHandlingCodeFragmentExecutor[Any] = null
-
-    script block = @{executor = new EventHandlingCodeFragmentExecutor(there, there.scriptExecutor)}: {. .}
-    def release = executor.executeMatching(isMatching=true)
-  }
-
-  private val callHandlers = ListBuffer[PartialFunction[Any, Unit]]()
-
-
-
-  // Scripts
-  script live: Any
-  script terminate = Terminator.block
-  script die       = {!if (context ne null) context stop self!}
-
-  script r$(handler: PartialFunction[Any, Script[Any]])
-  = var s:Script[Any]=null
-    @{var handlerWithExecuteAA = handler andThen {hr => {s = hr; there.eventHappened}}
-                          synchronized {callHandlers += handlerWithExecuteAA}
-      there.onDeactivate {synchronized {callHandlers -= handlerWithExecuteAA}}
-    }: {. Debug.info(s"$this.r$$") .}
-    if s != null then s
-
-
-
-  script lifecycle = [live || terminate] ; die
-  
-  // Callbacks
-  override def aroundPreStart() {
-    Debug.info(s"$this aroundPreStart INIT")  
-    runner.launch([lifecycle])
-    super.aroundPreStart()
-    Debug.info(s"$this aroundPreStart EXIT")
-  }
-
-  override def aroundReceive(receive: Actor.Receive, msg: Any) {
-    synchronized {
-      sendSynchronizationMessage(this)
-      wait()
-    }
-
-    callHandlers.synchronized { // TBD: why is synchronized needed
-      callHandlers.collectFirst { case handler if handler isDefinedAt msg => handler(msg) } match {
-        case None    => super.aroundReceive( receive        , msg); Debug.info(s"$this aroundReceive did NOT handle msg   sender: $sender msg: $msg")
-        case Some(_) => super.aroundReceive({case _: Any =>}, msg); Debug.info(s"$this aroundReceive handled  sender: $sender msg: $msg")
-      }
-    }
-  }
-
-  override def aroundPostStop() {
-    Terminator.release
-    super.aroundPostStop()
-  }
-
-  def receive: Actor.Receive = {case _ =>}
-
-  def sendSynchronizationMessage(lock: AnyRef) {
-    val vm = runner.executor
-    vm insert SynchronizationMessage(vm.rootNode, lock)
-  }
-}
-
-case class SynchronizationMessage(node: CallGraphNode, lock: AnyRef) extends CallGraphMessageN {
-  type N = CallGraphNode
-  override def priority = PRIORITY_InvokeFromET - 1  // After all actors are launched
 }
--- framework/core-scripts/src/main/scala/subscript/objectalgebra/Trigger.scala
+++ framework/core-scripts/src/main/scala/subscript/objectalgebra/Trigger.scala
@@ -4,12 +4,23 @@ import subscript.language
 
 class Trigger extends SSProcess {
   var listeners = List[() => Unit]()
-  def trigger = listeners.foreach(_())
   def addListener(f: () => Unit) {listeners ::= f}
 
-  script live = @{
+  private[this] var value: Any = null.asInstanceOf[Any]
+  
+  /** Sets the value and triggers the trigger. `v` will be return as a result value of this trigger. */
+  def trigger(v: Any) {
+    value = v
+    listeners.foreach(_())
+  }
+
+  /** Triggers this trigger with `null` as a result value. */
+  def trigger {trigger(null.asInstanceOf[Any])}
+
+  override script live = @{
     val listener = {() => there.codeExecutor.executeAA}
     addListener(listener)
     there.onDeactivate {listeners = listeners.filter(_ != listener)}
-  }: {. .}
+  }: {.value.}
+
 }
